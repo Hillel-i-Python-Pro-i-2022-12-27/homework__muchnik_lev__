50a008762d75b073f51dc8d1254817156298195f
Лев Мучник <lionrav19@gmail.com>
2023-01-15 13:53:16 +0200

    new main + application

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-def main():
-    print("Hello World")
+from random import randint
+import pandas as pd
+from faker import Faker
+
+fake = Faker(locale="ru_RU")
+def input_fakedata(x):
+
+        data = pd.DataFrame()
+        for i in range(0, x):
+            data.loc[i, 'id'] = randint(1, 100)
+            data.loc[i, 'name'] = fake.name()
+            data.loc[i, 'address'] = fake.address()
+            data.loc[i, 'latitude'] = str(fake.latitude())
+            data.loc[i, 'longitude'] = str(fake.longitude())
+        return data
 
 
-
-
-if __name__ == "__main__":
-    main()
-
+input_fakedata(10)
