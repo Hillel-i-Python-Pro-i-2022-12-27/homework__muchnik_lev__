28780e8f9db8c68af3e0f191c5879efadda629a0
Лев Мучник <lionrav19@gmail.com>
2023-01-15 21:20:33 +0200

    Added = fakeman, dock.ignore.,dock-comp.,Dock.File

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
-from random import randint
-import pandas as pd
-from faker import Faker
+from application import fakeman
 
-fake = Faker(locale="ru_RU")
-def input_fakedata(x):
-
-        data = pd.DataFrame()
-        for i in range(0, x):
-            data.loc[i, 'id'] = randint(1, 100)
-            data.loc[i, 'name'] = fake.name()
-            data.loc[i, 'address'] = fake.address()
-            data.loc[i, 'latitude'] = str(fake.latitude())
-            data.loc[i, 'longitude'] = str(fake.longitude())
-        return data
+def main() -> None:
+    print(fakeman)
 
 
-input_fakedata(10)
+if __name__ == "__main__":
+    main()
